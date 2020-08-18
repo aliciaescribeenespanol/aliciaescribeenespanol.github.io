a4ed9a01f64a92a2cd02b81986e89b20d23e9ece
aliciaescribeenespanol <69337847+aliciaescribeenespanol@users.noreply.github.com>
2020-08-18 14:38:42 -0700

    updatefooter

---
+++ postFunctions.js
@@ -0,0 +1,11 @@
+function get16Aug2020(){
+ document.getElementById("currentPost").innerHTML= "16 Agosto 2020 ¡Hoy aprendí hacer un post en el blog con una caja de texto! Fue un día soleado con temperaturas altas. Por eso, me quedé en casa por la mayor parte del día. Hice mucho té  verde con hielo. Aunque debo estudiar las matemáticas o algo con relación a los estudios escolares, decidí pasar el día  viendo YouTube y escribiendo un post.";
+}
+
+function get17Aug2020(){
+ document.getElementById("currentPost").innerHTML= "17 Agosto 2020 Hoy no hice mucho. Nuevamente, fue un día solado y monté en bicicleta a los parques en mi barrio. Aprendí cómo calcular la pendiente de una línea en tres dimensión como parte de la lectura que me presenta el libro de cálculo, es decir, la derivada. A la larga, no necesitaré todos esos concepto, pero es algo que hacer durante el verano. Me divierto escribir en español y me alegro de que sea posible hacer un blog en GitHub para seguir practicando mi redacción.";
+}
+
+function get18Aug2020(){
+ document.getElementById("currentPost").innerHTML= "18 Agosto 2020 ¡Buenos días! Acabé de ver el video de mi profesor de matemáticas. Hoy descubrimos las conexiones entre el teorema de Green y el de Stokes. Aunque las computaciones son fáciles, el concepto escondido en el fondo de las fórmulas y dibujos es bastante complejo. Sólo me falta una semana ante de que tenga tiempo libre. Por cierto, nuestra máquina de hielo se rompió ayer, y desafortunadamente, no sabemos cómo arreglarla.";
+}
