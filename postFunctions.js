95ea3fb884027d50991047a1f67470ac1fbb5012
aliciaescribeenespanol <69337847+aliciaescribeenespanol@users.noreply.github.com>
2020-08-21 13:12:28 -0700

    newIntro!

--- postFunctions.js
+++ postFunctions.js
@@ -9,3 +9,15 @@ function get17Aug2020(){
 function get18Aug2020(){
  document.getElementById("currentPost").innerHTML= "18 Agosto 2020 ¡Buenos días! Acabé de ver el video de mi profesor de matemáticas. Hoy descubrimos las conexiones entre el teorema de Green y el de Stokes. Aunque las computaciones son fáciles, el concepto escondido en el fondo de las fórmulas y dibujos es bastante complejo. Sólo me falta una semana ante de que tenga tiempo libre. Por cierto, nuestra máquina de hielo se rompió ayer, y desafortunadamente, no sabemos cómo arreglarla.";
 }
+
+function get19Aug2020(){
+ document.getElementById("currentPost").innerHTML= "19 Agosto 2020 Hemos arreglado la máquina de hielo. Ya no está soleado, y llovió anoche. Aprendí algo sobre CSS y cómo puedo hacer funcionar mi sitio web en un celular. Es fascinante ver las cosas que cambian para adaptar a una pantalla delgada. Por un lado, es bueno saber las diferencias que necesito tomar en cuenta cuando uso un programa escrito para computadoras en un celular. Por el otro lado, sé que hay redundancia en mi CSS porque no soy experta sino aprendiz.";
+}
+
+function get20Aug2020(){
+ document.getElementById("currentPost").innerHTML="20 Agosto 2020 ¡Hoy tomé la última prueba de matemáticas para este trimestre! He terminado de aprender todo el libro - muy grande - de las computaciones básicas de cálculo, pero no me siento satisfecha. Me doy cuenta de que hay más que aprender, más que descubrir. Aunque no me interesa especializarme en las matemáticas, confieso que la materia es una herramienta con muchas aplicaciones. Tampoco me interesa la computación cuando pertenece a las carreras. Sin embargo, he decidido aprender unos lenguajes de programación para crear mi propio blog en español en el que puedo practicar mi español durante los descansos.";
+}
+
+function get21Aug2020(){
+ document.getElementById("currentPost").innerHTML="21 Agosto 2020 No puedo creer que sólo haga falta veinte días antes de que regrese a la secundaria en línea. ¿Dónde está el verano? El tiempo está pasando como si fuera arena en mis manos. Mejor que empiece a repasar el español que me he olvidado durante los últimos meses. Será mi último año en la secundaria y estoy muy emocionada, pero no estoy lista. Necesito más tiempo de vacaciones. No he llenado ninguna solicitud para entrar en la universidad. No he tomado fotos para el anuario. No me he dedicado a una especialidad. Apenas he hecho funcionar este blog en los celulares - lo funciona, por lo menos. ¡Ay!";
+}
