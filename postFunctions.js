19dd223018ca3285641b3d2a276cffefc3f4d724
aliciaescribeenespanol <69337847+aliciaescribeenespanol@users.noreply.github.com>
2020-08-30 14:08:50 -0700

    cuentoUp

--- postFunctions.js
+++ postFunctions.js
@@ -1,53 +1,70 @@
 function get16Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML= "16 Agosto 2020 ¡Hoy aprendí hacer un post en el blog con una caja de texto! Fue un día soleado con temperaturas altas. Por eso, me quedé en casa por la mayor parte del día. Hice mucho té  verde con hielo. Aunque debo estudiar las matemáticas o algo con relación a los estudios escolares, decidí pasar el día  viendo YouTube y escribiendo un post.";
 }
 
 function get17Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML= "17 Agosto 2020 Hoy no hice mucho. Nuevamente, fue un día solado y monté en bicicleta a los parques en mi barrio. Aprendí cómo calcular la pendiente de una línea en tres dimensión como parte de la lectura que me presenta el libro de cálculo, es decir, la derivada. A la larga, no necesitaré todos esos concepto, pero es algo que hacer durante el verano. Me divierto escribir en español y me alegro de que sea posible hacer un blog en GitHub para seguir practicando mi redacción.";
 }
 
 function get18Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML= "18 Agosto 2020 ¡Buenos días! Acabé de ver el video de mi profesor de matemáticas. Hoy descubrimos las conexiones entre el teorema de Green y el de Stokes. Aunque las computaciones son fáciles, el concepto escondido en el fondo de las fórmulas y dibujos es bastante complejo. Sólo me falta una semana ante de que tenga tiempo libre. Por cierto, nuestra máquina de hielo se rompió ayer, y desafortunadamente, no sabemos cómo arreglarla.";
 }
 
 function get19Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML= "19 Agosto 2020 Hemos arreglado la máquina de hielo. Ya no está soleado, y llovió anoche. Aprendí algo sobre CSS y cómo puedo hacer funcionar mi sitio web en un celular. Es fascinante ver las cosas que cambian para adaptar a una pantalla delgada. Por un lado, es bueno saber las diferencias que necesito tomar en cuenta cuando uso un programa escrito para computadoras en un celular. Por el otro lado, sé que hay redundancia en mi CSS porque no soy experta sino aprendiz.";
 }
 
 function get20Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="20 Agosto 2020 ¡Hoy tomé la última prueba de matemáticas para este trimestre! He terminado de aprender todo el libro - muy grande - de las computaciones básicas de cálculo, pero no me siento satisfecha. Me doy cuenta de que hay más que aprender, más que descubrir. Aunque no me interesa especializarme en las matemáticas, confieso que la materia es una herramienta con muchas aplicaciones. Tampoco me interesa la computación cuando pertenece a las carreras. Sin embargo, he decidido aprender unos lenguajes básicos de programación para crear mi propio blog en español en el que puedo practicar mi español durante los descansos.";
 }
 
 function get21Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="21 Agosto 2020 No puedo creer que sólo haga falta veinte días antes de que regrese a la secundaria en línea. ¿Dónde está el verano? El tiempo está pasando como si fuera arena en mis manos. Mejor que empiece a repasar el español que me he olvidado durante los últimos meses. Será mi último año en la secundaria y estoy muy emocionada, pero no estoy lista. Necesito más tiempo de vacaciones. No he llenado ninguna solicitud para entrar en la universidad. No he tomado fotos para el anuario. No me he dedicado a una especialidad. Apenas he hecho funcionar este blog en los celulares - funciona, por lo menos. ¡Ay!";
 }
 
 function get22Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="22 Agosto 2020 Fui a la playa hoy. A mí no me encanta ir a la playa cuando hace sol y regresar a casa tarde. Para mí, fue una pérdida de tiempo. Prefiero montar en bicicleta a unos parques cercanos y correr en la pista de una escuela secundaria. Además, había demasiados platos; pasé tres horas lavándolos y ahora estoy cansada.";
 }
 
 function get23Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="23 Agosto 2020 Necesito una nueva foto para el blog. La que uso no cabe bien en mi celular. Sin embargo, no he tenido mucho tiempo libre esos últimos días y me gusta la foto de Perú como el fondo. Tal vez encontraré más fotos de la selva peruana tan deslumbrantes. Desafortunadamente, me quedan muchos platos que lavar aunque lavé un montón de ellos ayer. No obstante, lo haré eventualmente porque no hay nadie en mi casa que limpia la cocina de una manera eficaz.";
 }
 
 function get24Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="24 Agosto 2020 Pasé el día caminando por mi vecindario. Está nublado hoy, pero tengo calor. Mañana tendré un examen importante en la clase de cálculo. Será la última tarea y estoy muy emocionada. Después, podré disfrutar del resto del verano. Por cierto, compré un nombre en Google esta tarde para que cualquier persona pueda encontrar mi sitio web en una búsqueda. Por último, he cambiado de imagen para el fondo de una pantalla pequeña. Creo escoger un mono lindo.";
 }
 
 function get25Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="25 Agosto 2020 Acabé de entregar mi examen de matemáticas. Fue el más fácil de todos los examens que he tomado este trimestre. Tuve que calcular el flujo y la circulación de unas líneas en un campo de vectores usando tres teoremas. No sé cuándo necesitaré esas técnicas; quizás durante la clase de física que voy a tomar muy pronto. Ahora tengo que montar en bicicleta a la tienda de comestibes porque nuestra sopa, la que cenaremos en tres horas, falta los fideos.";
 }
 
 function get26Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="26 Agosto 2020 Tengo mucho tiempo libre. Entonces, empecé a leer un libro escrito por Sonia Sotomayor, la juez en la Corte Suprema de los Estados Unidos. Luego, voy a ir de compras para comprar queso para un plato italiano que vamos a hacer hoy. Aunque el supermercado está lejos, puede ser como entrenamiento para los deportes cuando sea posible practicarlos. Mi padre atrapó una rata anoche, pero se parece un ratón. Si tuviera una mascota, escogería un ratón porque son jugadores y inteligentes.";
 }
 
 function get27Aug2020(){
+document.getElementById("currentIm").style.display = "none";
  document.getElementById("currentPost").innerHTML="27 Agosto 2020 Cociné un montón de panqueques esta mañana porque no los he comido recientemente. Recibí mis calificaciones para el trimestre y por eso, me siento aliviada. En cuanto al blog, me gustaría probar unas maneras de pedir opiniones sobre mi escritura. Creo que un elemento vacío que un visitante puede cambiar sería la mejor manera de hacerlo para que el ordenador envie el texto a mi correo electrónico. Ojalá funcione.";
 }
 
 function get28Aug2020(){
+document.getElementById("currentIm").style.display = "block";
  document.getElementById("currentIm").src = "Imagenes/survey.JPG"; 
  document.getElementById("currentPost").innerHTML="28 Agosto 2020 Ayer decidí usar Google Forms para implementar una manera de recibir comentarios porque es difícil hacerlo sin aprender PHP, un lenguage esencial para guardar los datos y enviarlos a mi correo electrónico. También creé un correo electrónico con el que se puede enviarme cosas como opción alternativa a Google Forms. Quiero empezar a poner imagenes en mi blog; puede ser interesante.";
- 
+}
+
+function get29Aug2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML="29 Agosto 2020 Fui a la playa en una isla para hacer paddleboard. Ya hemos vuelto a casa y estoy muy cansada. Entonces, me voy a dormir.";
 }
