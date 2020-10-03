be5102a057a70edd02d622ca7ce27e0625841ef4
aliciaescribeenespanol <69337847+aliciaescribeenespanol@users.noreply.github.com>
2020-10-03 14:17:36 -0700

    newPost

---
+++ BlogPost/postOct2020.js
@@ -0,0 +1,15 @@
+function get1Oct2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "1 Octubre 2020 ¡Feliz nuevo mes! He estado ocupada con las clases, pero todo bien. Necesito arreglar mi sitio para que funcione mejor en una pantalla pequeña.";
+}
+
+function get2Oct2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "2 Octubre 2020 Mi familia y yo nos vamos a mudar pronto. He estado muy ocupada. Hay muchas cosas que hacer en la casa.";
+}
+
+function get3Oct2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "3 Octubre 2020 He sobrevivido la primera semana de las nuevas clases y quiero descansar. Sin embargo, limpiaré la estufa esta tarde. Si tengo tiempo, saldré para montar en bicicleta. Puede ser la última oportunidad de recorrer el barrio. Extrañaré este lugar.";
+}
+
