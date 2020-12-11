86d51026f4053e62b82843458fddc4388e631643
aliciaescribeenespanol <69337847+aliciaescribeenespanol@users.noreply.github.com>
2020-12-10 16:55:30 -0800

    postup

---
+++ BlogPost/postDec2020.js
@@ -0,0 +1,43 @@
+function get1Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "1 Diciembre 2020 ¡Feliz nuevo mes! Estoy contenta porque solo quedan tres semanas de escuela antes del descanso. Me va bastante bien. Por eso, puedo pasar unos minutos escribiendo cosas para la clase de español y para este blog.";
+}
+
+function get2Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "2 Diciembre 2020 Llovió anoche. Hace mucho frío hoy. Pensé que cayó granizo esta mañana. No tengo clases hoy, aparte de la clase de español.";
+}
+
+function get3Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "3 Diciembre 2020 Estoy cansada. Fui a la escuela para recoger un yoyó. Es para la física. Estamos aprendiendo acerca de el movimiento rotacional...en una clase virtual.";
+}
+function get4Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "4 Diciembre 2020 ¡Viernes! Me alegra de que hoy sea viernes.";
+}
+function get5Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "5 Diciembre 2020 Hoy me doy cuenta de que no hemos recibido el correo hace una semana. Resultó que nuestro correo fue entregado a otra casa. Eso puede ser un problema.";
+}
+function get6Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "6 Diciembre 2020 Esta semana tendré tres exámenes porque es la última semana de clases. Estoy muy emocionada. Sin embargo, debo estudiar antes de celebrar.";
+}
+
+function get7Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "7 Diciembre 2020 Escuchamos unos villancicos en la clase de español. Quería practicar la palabra <<villancico>> porque es nueva para mí. Después, estudié unos temas de la literatura española, específicamente la influencia de la clase social con relación del propósito de una obra. Fue interesante.";
+}
+function get8Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "8 Diciembre 2020 He pensado en jugar Scrabble en línea. Se me occure esta idea cuando estaba viendo unos videos en YouTube y vi un anuncio para un juego de palabras. Hay un club en línea y he jugado Scrabble en inglés. Pero, no sé muchas palabras en español. Tal vez intentaré durante el descanso este diciembre.";
+}
+function get9Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "9 Diciembre 2020 Hoy se presenta el examen de las ecuaciones diferencias. Pienso que esta clase es fácil y el examen fue fácil. He acabado de tomar todas las clases de matemáticas que ofrece el colegio, excepto las estadísticas. Seguimos con las clases en línea, así que no voy a tomar las estadísticas hasta que regresemos al colegio. Prefiero aprender las matemáticas en un salón de clase y puedo esperar. Entre tanto, puedo estudiar la probabilidad. Pero, la estudiaré después del español porque el español es muy importante.";
+}
+function get10Dec2020(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "10 Diciembre 2020 El distrito ha cambiado de los horarios. Me disgusta mucho porque tengo que arreglar mi horario para que las clases no sean a la misma hora. La otra opción es saltarme una clase, la cual no es buena.";
+}
