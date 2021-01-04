b752f5a8dad5098772b78f0faaa74146290023f2
aliciaescribeenespanol <69337847+aliciaescribeenespanol@users.noreply.github.com>
2021-01-03 17:33:21 -0800

    updatefluff

--- general.js
+++ general.js
@@ -1,3 +1,14 @@
 function headingColor(id,color){
 document.getElementById(id).style.background-color="color";
 }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
