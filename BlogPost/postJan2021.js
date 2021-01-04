d00c55c0c9209806418079d31c1814b4551176a0
aliciaescribeenespanol <69337847+aliciaescribeenespanol@users.noreply.github.com>
2021-01-03 20:20:22 -0800

    mobile

---
+++ BlogPost/postJan2021.js
@@ -0,0 +1,14 @@
+function get1Jan2021(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "1 Enero 2021 El año nuevo ha llegado. Espero que 2021 sea mejor que 2020.";
+}
+
+function get2Jan2021(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "2 Enero 2021 No puedo creer que el desanso termine dentro de dos días. Necesito descansar un poco más.";
+}
+
+function get3Jan2021(){
+document.getElementById("currentIm").style.display = "none";
+ document.getElementById("currentPost").innerHTML= "3 Enero 2021 Tendré nuevas clases mañana. Pero, debo empezar a leer San Manuel Bueno, mártir. Me parece interesante y no he leído mucho recientemente.";
+}
